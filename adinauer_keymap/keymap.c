b7eed7c326cd82e0db641d9475df09092278b981
Alexander Dinauer <alexander@dinauer.at>
2016-05-29 21:45:20 +0200

    Swap - and \ keys as - is required more often.

--- adinauer_keymap/keymap.c
+++ adinauer_keymap/keymap.c
@@ -14,9 +14,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* layer 0 : default
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   `    |   1  |   2  |   3  |   4  |   5  |  -   |           |   =  |   6  |   7  |   8  |   9  |   0  | BSpace |
+ * |   `    |   1  |   2  |   3  |   4  |   5  |  \   |           |   =  |   6  |   7  |   8  |   9  |   0  | BSpace |
  * |--------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  |  [   |           |   ]  |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |  [   |           |   ]  |   Y  |   U  |   I  |   O  |   P  |   -    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | ~L4/Esc|   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
  * |--------+------+------+------+------+------| ~L5  |           | +L5  |------+------+------+------+------+--------|
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [BASE] = KEYMAP(
         // left hand
-        KC_GRV,         KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_MINS,
+        KC_GRV,         KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_BSLS,
         KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_LBRC,
         LT(4, KC_ESC),  KC_A,           KC_S,           KC_D,           KC_F,           KC_G,
         KC_LSFT,        LT(1, KC_Z),    KC_X,           KC_C,           KC_V,           KC_B,           MO(5),
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                         KC_SPC,         KC_ENT,         KC_BSPC,
         // right hand
         KC_EQL,         KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_BSPC,
-        KC_RBRC,        KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLS,
+        KC_RBRC,        KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_MINS,
                         KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        KC_QUOT,
         TG(5),          LT(4, KC_N),    KC_M,           KC_COMM,        KC_DOT,         LT(1, KC_SLSH), KC_RSFT,
                                         KC_RGUI,        MO(2),          MO(2),          MO(3),          KC_RCTL,  
