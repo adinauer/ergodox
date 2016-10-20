0620b37728153721bb36fd037ea3e2d07ba3a7eb
Alexander Dinauer <alexander.dinauer@bytepoets.com>
2016-10-20 12:13:50 +0200

    Fix mouse speed and tap delay
    Rename config_user.h to config.h

--- adinauer_keymap/config.h
+++ adinauer_keymap/config.h
@@ -1,7 +1,10 @@
 #ifndef CONFIG_USER_H
 #define CONFIG_USER_H
 
-#include "config.h"
+#include "../../config.h"
+
+#undef MOUSEKEY_MAX_SPEED
+#undef TAPPING_TERM
 
 #define MOUSEKEY_MAX_SPEED 10
 #define TAPPING_TERM 150
