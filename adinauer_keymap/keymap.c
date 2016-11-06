cd356cdacd5e845cde70d4acbf375b959f8855cd
Alexander Dinauer <alexander@dinauer.at>
2016-11-06 23:36:34 +0100

    Add GAME layer which has some modifiers disabled and replaces RGUI on right thumb with CTRL

--- adinauer_keymap/keymap.c
+++ adinauer_keymap/keymap.c
@@ -5,10 +5,12 @@
 #define BASE 0 // default layer
 #define SPCL 1 // special characters
 #define MOUS 2 // mouse
-#define FKEY 3 // F-keys
+#define GAME 3 // gaming / fewer modifiers
 #define NAVI 4 // navigation
 #define NUMB 5 // numpad
 #define MPTY 9 // empty (copy as template)
+
+// DOCS: https://github.com/jackhumbert/qmk_firmware
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* layer 0 : default
@@ -25,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *   | Ctrl |  ~L3 | Caps | LALT | LGUI |                                       | RGUI |  ~L4 |  ~L2 |  ~L3 | RCtrl|
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        |      | -L0  |       | Vol- | Vol+ |
+ *                                        |  L3  | -L0  |       | Vol- | Vol+ |
  *                                 ,------|------+------|       |------+------+------.
  *                                 |      |      |      |       | Mute |      |      |
  *                                 |Space | Enter|------|       |------| Alt  | LGUI |
@@ -39,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LT(4, KC_ESC),  KC_A,           KC_S,           KC_D,           KC_F,           KC_G,
         KC_LSFT,        LT(1, KC_Z),    KC_X,           KC_C,           KC_V,           LT(2, KC_B),    MO(5),
         KC_LCTL,        MO(3),          KC_CAPS,        KC_LALT,        KC_LGUI,
-                                                                                        KC_NO,          M(100),
+                                                                                        TG(3),          M(100),
                                                                                                         KC_NO,
                                                                         KC_SPC,         KC_ENT,         KC_BSPC,
         // right hand
@@ -137,46 +139,46 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS,        KC_TRNS,        KC_TRNS
     ),
 
-/* layer 3 : F-Keys
+/* layer 3 : gaming / fewer modifiers
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   F11  |  F1  |  F2  |  F3  |  F4  |  F5  |  F11 |           |  F12 |  F6  |  F7  |  F8  |  F9  |  F10 |   F12  |
+ * | Esc    |   1  |   2  |   3  |   4  |   5  |  \   |           |   =  |   6  |   7  |   8  |   9  |   0  | BSpace |
  * |--------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |  [   |           |   ]  |   Y  |   U  |   I  |   O  |   P  |   -    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |   F11  |  F1  |  F2  |  F3  |  F4  |  F5  |------|           |------|  F6  |  F7  |  F8  |  F9  |  F10 |   F12  |
- * |--------+------+------+------+------+------|  F11 |           |  F12 |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * | ~L4    |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      |      |      |                                       |      |      |      |      |      |
+ *   | RCtrl|  ~L3 | Caps | LALT | LGUI |                                       | RGUI |  ~L4 |  ~L2 |  ~L3 | RCtrl|
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        |      |      |       |      |      |
+ *                                        |      | -L0  |       | Vol- | Vol+ |
  *                                 ,------|------+------|       |------+------+------.
- *                                 |      |      |      |       |      |      |      |
- *                                 |      |      |------|       |------|      |      |
- *                                 |      |      |      |       |      |      |      |
+ *                                 |      |      |      |       | Mute |      |      |
+ *                                 |Space | Enter|------|       |------| Alt  | RCtrl|
+ *                                 |      |      |BSpace|       |  Esc |      |      |
  *                                 `--------------------'       `--------------------'
  */
-    [FKEY] = KEYMAP(
+    [GAME] = KEYMAP(
         // left hand
-        KC_F11,         KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_F11,
-        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,
-        KC_F11,         KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,    
-        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_F11,
-        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,    
-                                                                                        KC_TRNS,        KC_TRNS,
-                                                                                                        KC_TRNS,
-                                                                        KC_TRNS,        KC_TRNS,        KC_TRNS,
+        KC_ESC,         KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_BSLS,
+        KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_LBRC,
+        LT(4, KC_ESC),  KC_A,           KC_S,           KC_D,           KC_F,           KC_G,
+        KC_LSFT,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_NO,
+        KC_RCTL,        MO(3),          KC_CAPS,        KC_LALT,        KC_LGUI,
+                                                                                        KC_NO,          M(100),
+                                                                                                        KC_NO,
+                                                                        KC_SPC,         KC_ENT,         KC_BSPC,
         // right hand
-        KC_F12,         KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F12,
-        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,
-                        KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F12,
-        KC_F12,         KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,
-                                        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,  
-        KC_TRNS,        KC_TRNS,
-        KC_TRNS,
-        KC_TRNS,        KC_TRNS,        KC_TRNS
+        KC_EQL,         KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_BSPC,
+        KC_RBRC,        KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_MINS,
+                        KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        KC_QUOT,
+        KC_NO,          KC_N,           KC_M,           KC_COMM,        KC_DOT,         KC_SLSH,        KC_RSFT,
+                                        KC_RGUI,        MO(4),          MO(2),          MO(3),          KC_RCTL,  
+        KC_VOLD,        KC_VOLU,
+        KC_MUTE,
+        KC_ESC,         KC_LALT,        KC_RCTL
     ),
 
 /* layer 4 : navigation keys
